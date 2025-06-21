31b13887da7120cd66353da5536acf32991c7138
PritamTheCoder <147150619+PritamTheCoder@users.noreply.github.com>
2025-06-21 17:58:59 +0545

    toggle side bar JS code update

---
+++ pages/JS/self-test.js
@@ -0,0 +1,8 @@
+// toggle sidebar visibility
+function toggleSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    const Overlay = document.querySelector('.Overlay');
+    sidebar.classList.toggle('active');
+    Overlay.classList.toggle('Overlay');
+}
+
